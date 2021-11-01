e31a5f003d4aa53522cb99a9fa49af996f67a1ba
Kübra Özdemir <92569107+kbraozdemir@users.noreply.github.com>
2021-11-01 20:41:57 +0300

    Add files via upload
    Ödev için gerekli GitHub linki açıldı.

---
+++ Program/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("İsminizi Girin: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyisminizi Girin: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Hoşgeldiniz " + name + " " + surname);
+            Console.ReadLine();
+        }
+    }
+}
