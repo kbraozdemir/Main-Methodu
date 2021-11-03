f96cb0f330393604f1dda8a8ede4c23f424d2279
Kübra Özdemir <92569107+kbraozdemir@users.noreply.github.com>
2021-11-04 01:01:34 +0300

    if_else_if dersi ödevi

---
+++ hata_yonetimi/karar_yapilari/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace karar_yapilari
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time<11)
+                Console.WriteLine("Günaydın!");
+            else if (time <= 18)
+                Console.WriteLine("İyi Günler!");
+            else
+                Console.WriteLine("İyi Geceler!");
+            string sonuc = time <= 18 ? "İyi Günler!" : "İyi Geceler";
+
+            sonuc = time >= 6 && time < 11 ? "Günaydın!" : time <= 10 ? "İyi Günler!" : "İyi Geceler!";
+            
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+        }
+    }
+}
